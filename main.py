76993847fee903767376fb4cfed9572282c0e287
bieltv3 <bieltv.3@gmail.com>
2018-11-15 10:11:10 +0000

    Add new audio files and new output

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ import sys, os
 # Timer dependencies
 from timeit import default_timer as timer
 
-t_i = timer()
-
 # Packages import
+t_i = timer()
 import detect_keyword as dk
-
-# Detect the first word
-word = dk.detect(sys.argv[1])
 t_f = timer() - t_i
-print("Keyword detected as " + str(word) + " - Time: " + str(t_f))
+
+print("Loaded all modeules" + "\tTime: %.3f" % t_f)
+
+
 
 # Detect the second word
 t_i = timer()
-word = dk.detect(sys.argv[2])
+#word = dk.detect(sys.argv[2])
 t_f = timer() - t_i
-print("Keyword detected as " + str(word) + " - Time: " + str(t_f))
+#print("Keyword detected as " + str(word) + " - Time: " + str(t_f))
+
+# Detect the word
+for file in sorted(os.listdir(sys.argv[1])):
+    t_i = timer()
+    word = dk.detect(sys.argv[1]+file)
+    t_f = timer() - t_i
+    print("Detection: " + str(word) + "\tTime: %.3f" % t_f + "\t" + file)
